e08507aaa7cdf36924d8ee21529514995a427bb9
Vasili1990 <vasili.tsiskarishvili04bp@geolab.edu.ge>
2020-02-20 18:54:27 +0400

    while true
    while true play again functional

--- Dice_game/dice_game_main.py
+++ Dice_game/dice_game_main.py
@@ -3,24 +3,32 @@ def dice_roll():
     x = random.randint(1, 6)
     return x
 
-numoftr = int(input("Numer of tries: "))
 
-Player = dice_roll()
-Computer = dice_roll()
 
-i = 0
-while i < numoftr:
-    Player = dice_roll()
-    Computer = dice_roll()
-    if Player > Computer:
-        print("You won")
-        print("Player: " + str(Player) + " / " + "Computer: " + str(Computer))
-    elif Player == Computer:
-        print("Draw")
-        print("Player: " + str(Player) + " / " + "Computer: " + str(Computer))
-    elif Player < Computer:
-        print("You lost")
-        print("Player: " + str(Player) + " / " + "Computer: " + str(Computer))
+while True:
+    x = input("do you want to play: ")
+    if x in ["yes","Yes","ok"]:
+         numoftr = int(input("Numer of tries: "))
+
+         Player = dice_roll()
+         Computer = dice_roll()
+         i = 0
+         while i < numoftr:
+            Player = dice_roll()
+            Computer = dice_roll()
+            if Player > Computer:
+                print("You won")
+                print("Player: " + str(Player) + " / " + "Computer: " + str(Computer))
+            elif Player == Computer:
+                print("Draw")
+                print("Player: " + str(Player) + " / " + "Computer: " + str(Computer))
+            elif Player < Computer:
+                print("You lost")
+                print("Player: " + str(Player) + " / " + "Computer: " + str(Computer))
+            else:
+                print("Try again")
+            i = i +1
     else:
-        print("Try again")
-    i = i +1
+        break          
+
+    
